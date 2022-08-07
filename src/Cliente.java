7f2ed8a955050d382d7f6f9eb4b67572df362da9
Gregory <gregbanegas.lovo@gmail.com>
2022-08-07 10:30:42 -0600

    Avance 2

--- src/Cliente.java
+++ src/Cliente.java
@@ -4,6 +4,8 @@ public class Cliente {
     String NumeroCelular;
     Boolean Sexo;
 
+    //constructor
+
     public Cliente(String nombre, String identidad, String numeroCelular, Boolean sexo) {
         Nombre = nombre;
         Identidad = identidad;
@@ -11,24 +13,40 @@ public class Cliente {
         Sexo = sexo;
     }
 
+    //sobrecarga
+    public Cliente(String nombre, String identidad, Boolean sexo) {
+        Nombre = nombre;
+        Identidad = identidad;
+        //NumeroCelular = numeroCelular;
+        Sexo = sexo;
+    }
+
+    //metodos
+
     public String getNombre() {
         return Nombre;
     }
 
     public String getIdentidad() {
+
         return Identidad;
     }
 
     public String getNumeroCelular() {
+
         return NumeroCelular;
     }
 
     public void setNumeroCelular(String numeroCelular) {
+
         NumeroCelular = numeroCelular;
     }
 
     public String getSexo() {
         return Sexo?"Femenino":"Masculino";
+        // si sexo es verdadero "Femenino" si es falso "Masculino"
+        // if()
+
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        Cliente cl = new Cliente("Eleana", "030120002760","94735251",true);
-        System.out.println(cl.getNombre());
-        System.out.println(cl.getIdentidad());
-        System.out.println(cl.getNumeroCelular());
-        System.out.println(cl.getSexo());
 
+        Cliente cl1 = new Cliente("Fernanda", "030120002760","94735251",true);
+        Cliente cl2 = new Cliente("Gregory", "030120002760","94735251",false);
+        Cliente cl3 = new Cliente("Julio", "030120002760","94735251",false);
+        Cliente cl4 = new Cliente("Rosa", "030120002760","94735251",true);
+        Cliente cl5 = new Cliente("Edwin", "030120002760",false);
+
+        Cliente[] listaCliente= new Cliente[5];
+        listaCliente[0] = cl1;
+        listaCliente[1] = cl2;
+        listaCliente[2] = cl3;
+        listaCliente[3] = cl4;
+        listaCliente[4] = cl5;
+
+        for(int i=0;i<listaCliente.length;i++){
+            System.out.println(listaCliente[i].getNombre());
+        }
+
+        Encurtido encurtido = new Encurtido(2022,"Cebolla, Chile, Zanahoria",1,false,120);
+        Encurtido encurtido2 = new Encurtido(2023,"Cebolla, Chile, Zanahoria",2,true,125);
+        Encurtido encurtido3 = new Encurtido(2024,"Cebolla, Chile, Zanahoria",1,true,120);
+        Encurtido encurtido4 = new Encurtido(2025,"Cebolla, Chile, Zanahoria, Rabano",1,true,130);
+        Encurtido encurtido5 = new Encurtido(2026,"repollo ",1,false,80);
+
+        Encurtido [] listaEncurtido = new Encurtido[5];
+        listaEncurtido[0] = encurtido;
+        listaEncurtido[1] = encurtido2;
+        listaEncurtido[2] = encurtido3;
+        listaEncurtido[3] = encurtido4;
+        listaEncurtido[4] = encurtido5;
+
+        for(int i = 0;i<listaCliente.length;i++){
+            System.out.println(listaEncurtido[i].getCod()+" "+listaEncurtido[i].getIngredientes());
+        }
     }
+
 
 
 }
